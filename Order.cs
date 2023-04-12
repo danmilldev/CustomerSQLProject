01d1c91250d1b814da2006848b153094397c4ca9
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-04-12 17:36:13 +0200

    Initial commit

---
+++ Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSQLProject
+{
+    public class Order
+    {
+
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CustomerSQLProject
+ 
